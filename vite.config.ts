4c2e855287b555ab0dd0dbc97238b670ba91f2a0
Alexzzl <805119233@qq.com>
2026-03-25 11:39:13 +0800

    初始化项目结构，添加配置文件，更新依赖，移除不必要的文件

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  build: {
+    outDir: 'dist',
+    target: 'es2015',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'mock-data': ['./src/data/mock.ts'],
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+        },
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
